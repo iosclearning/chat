0dc6468e4b1a18d47c2b47da9374db87c88d4a3f
Anel Memić <anelmemija@gmail.com>
2017-01-09 13:51:30 +0100

    Log In and Sign Up
    Basic functionality for Log In and Sign Up has been added.

---
+++ ApiMobileChat/LogInController.h
@@ -0,0 +1,25 @@
+//
+//  LogInController.h
+//  ApiMobileChat
+//
+//  Created by Anel Memic on 1/4/17.
+//  Copyright © 2017 api. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *EmailLabel;
+@property (strong, nonatomic) IBOutlet UITextField *EmailTextField;
+@property (strong, nonatomic) IBOutlet UILabel *EmailError;
+@property (strong, nonatomic) IBOutlet UILabel *PasswordLabel;
+@property (strong, nonatomic) IBOutlet UITextField *PasswordTextField;
+@property (strong, nonatomic) IBOutlet UILabel *PasswordError;
+@property (strong, nonatomic) IBOutlet UIButton *LoginButton;
+@property (strong, nonatomic) IBOutlet UILabel *SignUpLabelOne;
+@property (strong, nonatomic) IBOutlet UILabel *SignUpLabelTwo;
+@property (strong, nonatomic) IBOutlet UIButton *SignUpButton;
+@property (strong, nonatomic) NSDictionary *response;
+
+@end
